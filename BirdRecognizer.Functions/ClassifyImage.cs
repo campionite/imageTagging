25f8f4c7aecef3d664c0651da98adc90302620e8
Hina Khan <hk202010@outlook.com>
2020-11-07 18:00:50 -0500

    Azure function works now..

--- BirdRecognizer.Functions/ClassifyImage.cs
+++ BirdRecognizer.Functions/ClassifyImage.cs
@@ -14,7 +14,7 @@ namespace BirdRecognizer.Functions
     public static class ClassifyImage
     {
         [FunctionName("ClassifyImage")]
-        public static async Task Run([BlobTrigger("birds/{name}", 
+        public static async Task Run([BlobTrigger("clothes/{name}", 
                                      Connection = "AzureWebJobsStorage")]Stream blob, 
                                      string name)
         {
@@ -33,9 +33,12 @@ namespace BirdRecognizer.Functions
             var storageService = new AzureBlobStorageService(
                 new AzureBlobStorageServiceOptions
                     {
-                        ConnectionString = Environment.GetEnvironmentVariable("AzureWebJobsStorage"),
-                        ContainerName = Environment.GetEnvironmentVariable("ContainerName")
-                    });
+                    //ConnectionString = Environment.GetEnvironmentVariable("AzureWebJobsStorage"),
+                    //ContainerName = Environment.GetEnvironmentVariable("ContainerName")
+                    ConnectionString = "DefaultEndpointsProtocol=https;AccountName=imagetaggingstorage;AccountKey=anknBcg55WJ2N3+EbR5NVXiYwCYQRET/y05XelX5i1vIhZEupJvZa7BURZ0XRPtv3dpF4b8GmPbrt0NmqHOM5w==;EndpointSuffix=core.windows.net",
+                    ContainerName = "clothes"
+                        
+                });
             return storageService;
         }
 
@@ -46,20 +49,59 @@ namespace BirdRecognizer.Functions
                 != Common.ImageClassificationStatus.Pending.ToString();
         }
 
+        private static async Task<PredictionResponse> GetPredictionResponse_New(Stream blob)
+        {
+            byte[] byteData = GetImageAsByteArrayLocal(@"D:\Hackathon\images\test\hello.jpg");
+            using (var client = new HttpClient())
+            {
+                var url = "https://eastus.api.cognitive.microsoft.com/customvision/v3.0/Prediction/83e95659-edce-42ee-879e-8d2659bb870d/classify/iterations/Iteration1/image";
+
+                client.BaseAddress = new Uri(url);
+                client.DefaultRequestHeaders.Add("Prediction-Key",
+                "af5a9a9e832e4ec4b964bea4bfc70a25");
+                client.DefaultRequestHeaders
+                        .Accept
+                        .Add(new MediaTypeWithQualityHeaderValue("application/octet-stream"));
+                using (var request = new HttpRequestMessage(HttpMethod.Post, client.BaseAddress))
+                {
+                    request.Content = new ByteArrayContent(byteData);
+                    request.Content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+
+                    var response = await client.PostAsync(request.RequestUri, request.Content);
+                    var responseString = await response.Content.ReadAsStringAsync();
+                    return JsonConvert.DeserializeObject<PredictionResponse>(responseString);
+                }
+            }
+
+
+
+
+            
+        }
+
         private static async Task<PredictionResponse> GetPredictionResponse(Stream blob)
         {
             var client = new HttpClient();
-            client.DefaultRequestHeaders.Add("Prediction-Key", 
-                Environment.GetEnvironmentVariable("PredictionKey"));
+            //client.DefaultRequestHeaders.Add("Prediction-Key", 
+            //    Environment.GetEnvironmentVariable("PredictionKey"));
 
-            var url = Environment.GetEnvironmentVariable("PredictionUrl");
+            client.DefaultRequestHeaders.Add("Prediction-Key",
+                "af5a9a9e832e4ec4b964bea4bfc70a25");
+
+
+            var url = "https://eastus.api.cognitive.microsoft.com/customvision/v3.0/Prediction/83e95659-edce-42ee-879e-8d2659bb870d/classify/iterations/Iteration1/image";
+            //Environment.GetEnvironmentVariable("PredictionUrl");
 
             var byteData = GetImageAsByteArray(blob);
+            //byte[] byteData = GetImageAsByteArrayLocal(@"D:\Hackathon\images\test\hello.jpg");
+
 
             using (var content = new ByteArrayContent(byteData))
             {
-                content.Headers.ContentType = 
+                content.Headers.ContentType =
                     new MediaTypeHeaderValue("application/octet-stream");
+                //content.Headers.ContentType =
+                //    new MediaTypeHeaderValue("application/json");
                 var response = await client.PostAsync(url, content);
                 var responseString = await response.Content.ReadAsStringAsync();
                 return JsonConvert.DeserializeObject<PredictionResponse>(responseString);
@@ -72,6 +114,12 @@ namespace BirdRecognizer.Functions
             return binaryReader.ReadBytes((int)blob.Length);
         }
 
+        private static byte[] GetImageAsByteArrayLocal(string imageFilePath)
+        {
+            FileStream fileStream = new FileStream(imageFilePath, FileMode.Open, FileAccess.Read);
+            BinaryReader binaryReader = new BinaryReader(fileStream);
+            return binaryReader.ReadBytes((int)fileStream.Length);
+        }
         private static async Task ApplyPredictionToBlob(IStorageService storageService, 
                                                         string name, 
                                                         PredictionResponse response)
